a1893c4d376becb8716c313277907f4668f4952d
Daniel <lanigela@gmail.com>
2017-06-30 12:28:03 -0400

    put clara player inside widget

---
+++ web/js/clara-player.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright © Exocortex, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+  'jquery',
+  'claraplayer'
+  'jquery/ui',
+], function($, claraPlayer) {
+  $.widget('clara.player', {
+    options: {
+
+    },
+
+    _init: function () {
+
+    },
+
+    _create: function () {
+      var clara = player('clara-player');
+      clara.on('loaded', function() {
+        console.log('Clara player is loaded and ready');
+      });
+
+      // Fetch and initialize the sceneId
+      clara.sceneIO.fetchAndUse("6cfce43b-fd40-4624-bee6-7f911e9405dd");
+    },
+
+    _OnClick: function ($this, $widget) {
+
+    }
+
+  });
+
+  return $.clara.player;
+});
