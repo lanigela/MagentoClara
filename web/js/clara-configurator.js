4449ad3a707b50535c60cd480043d15fa1d4198e
Daniel <lanigela@gmail.com>
2017-07-10 12:13:14 -0400

    add clara configurator widget

---
+++ web/js/clara-configurator.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright © Exocortex, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+  'jquery',
+  'underscore',
+  'mage/template',
+  'mage/smart-keyboard-handler',
+  'mage/translate',
+  'priceUtils',
+  'claraplayer'
+  'jquery/ui',
+  'jquery/jquery.parsequery',
+  'mage/validation/validation'
+], function($, _, mageTemplate,  keyboardHandler, $t, priceUtils, claraPlayer) {
+  $.widget('clara.ConfiguratorRender', {
+    options: {
+
+    },
+
+    _init: function () {
+
+    },
+
+    _OnClick: function ($this, $widget) {
+
+    }
+
+  });
+
+  return $.clara.ConfiguratorRender;
+});
