b2988ccd800d213897ecfa20393781b2df515764
Daniel <lanigela@gmail.com>
2017-07-11 16:09:05 -0400

    counter

--- web/js/clara-configurator.js
+++ web/js/clara-configurator.js
@@ -31,23 +31,27 @@ define([
       console.log(this.options.optionConfig);
 
       var optionObj = this.options.optionConfig.options;
-
+      var optionCounter=1;
+      var selectionCounter=1;
       for(var key in optionObj) {
         // add div
         this.element.append('<div class="nested options-list">');
         // add title
         this.element.append('<label>' + optionObj[key].title + '</label>');
         // add selections
+        selectionCounter=1;
         for(var sel in optionObj[key].selections) {
           this.element.append('<div class="field choice">');
-          this.element.append('<input type="radio" class="radio product bundle" name="bundle_option[' + sel + ']" value="' + sel + '"/>');
+          this.element.append('<input type="radio" class="radio product bundle" name="bundle_option[' + optionCounter + ']" value="' + selectionCounter + '"/>');
           this.element.append('<label>' + optionObj[key].selections[sel].name + '</label>');
           this.element.append('</div>');
+          selectionCounter++;
         }
         // add option quantity
-        this.element.append('<input type="number" name="bundle_option_qty["' + key + ']" value="1"/>');
+        this.element.append('<input type="number" name="bundle_option_qty[' + optionCounter + ']" value="1"/>');
         // end div
         this.element.append('</div>');
+        optionCounter++;
       }
       console.log("done");
     }
