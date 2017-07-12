53e774f668f6497388d9c00a6bdc89587e7cce0d
Daniel <lanigela@gmail.com>
2017-07-12 16:05:32 -0400

    test cillow

--- web/js/clara-configurator.js
+++ web/js/clara-configurator.js
@@ -33,12 +33,76 @@ define([
       var clara = claraPlayer('clara-player');
       clara.on('loaded', function() {
         console.log('Clara player is loaded and ready');
+
+        ['orbit', 'pan', 'zoom'].forEach(function (tool) {
+          clara.player.hideTool(tool);
+        });
+        clara.configuration.initConfigurator({ form: 'Default', el: this.element });
+      });
+      clara.on('configurationChange', function (ev1) {
+        // api.player.frameScene();
+
+        // avoid infinite recursion
+        if (selfConfigChange && (ev1[0].name === 'Shape')) {
+          selfConfigChange = false;
+          return;
+        }
+
+        var config = clara.configuration.getConfiguration();
+
+        // back pillows allow depth up to 45cm
+        if (ev1[0].name === 'Pillow Type') {
+          var maxDepth = 25;
+          if (config['Pillow Type'] === 'Back' || config['Pillow Type'] === 'Back (Angled)') {
+            maxDepth = 45;
+          }
+          clara.configuration.setAttribute('Depth (cm)', { maxValue: maxDepth });
+        }
+
+        var widthHalf = (Number(config['Width (cm)']) - defaultDimensions.Width) / 100 / 2;
+        var lengthHalf = (Number(config['Length (cm)']) - defaultDimensions.Length) / 100 / 2;
+        var depthHalf = (Number(config['Depth (cm)']) - defaultDimensions.Depth) / 100 / 2;
+
+        // ***
+        ['bottom*', 'back*', 'mesh_elastic'].forEach(function (nodeName) { // add pocket
+          clara.scene.setAll({ name: nodeName, plug: 'PolyMesh', properties: { name: 'StretchX' }, property: 'stretchDistance' },
+            widthHalf);
+        });
+
+        ['bottom*', 'back*'].forEach(function (nodeName) { // add pocket
+          clara.scene.setAll({ name: nodeName, plug: 'PolyMesh', properties: { name: 'StretchY' }, property: 'stretchDistance' },
+            lengthHalf);
+        });
+
+        ['bottom*', 'back*'].forEach(function (nodeName) {
+          clara.scene.setAll({ name: nodeName, plug: 'PolyMesh', properties: { name: 'StretchZ' }, property: 'stretchDistance' },
+            depthHalf);
+        });
+
+        // api.scene.setAll({ name: 'Loops', plug: 'Transform', property: 'translation' }, new THREE.Vector3(widthHalf, 0, -lengthHalf));
+        // api.scene.setAll({ name: 'Loops2', plug: 'Transform', property: 'translation' }, new THREE.Vector3(-widthHalf, 0.0884, -lengthHalf));
+
+        clara.scene.setAll({ name: 'Stretch_Offset_Z', plug: 'Transform', property: 'translation' }, new THREE.Vector3(0, 0, -depthHalf));
+        clara.scene.setAll({ name: 'Stretch_Offset_XZ', plug: 'Transform', property: 'translation' }, new THREE.Vector3(widthHalf, 0, -depthHalf));
+        clara.scene.setAll({ name: 'Stretch_Offset_-XZ', plug: 'Transform', property: 'translation' }, new THREE.Vector3(-widthHalf, 0, -depthHalf));
+        // api.scene.setAll({ name: 'Stretch_Offset2', plug: 'Transform', property: 'translation' }, new THREE.Vector3(0, 0, -depthHalf));
+
+
+        if (ev1[0].name === 'Pillow Type' && config['Pillow Type'] !== 'Bottom') {
+          selfConfigChaqnge = true;
+          clara.scene.setAll({ name: 'Shear_Offset_Left', plug: 'Transform', property: 'translation' }, new THREE.Vector3(0, 0, 0));
+          clara.scene.setAll({ name: 'Shear_Offset_Right', plug: 'Transform', property: 'translation' }, new THREE.Vector3(0, 0, 0));
+        }
+        else {
+          selfConfigChange = true; // avoid infinite recursion
+          clara.configuration.executeAttribute('Shape', config['Shape']);
+        }
       });
 
       // Fetch and initialize the sceneId
-      clara.sceneIO.fetchAndUse(this.options.claraUUID);
+      clara.sceneIO.fetchAndUse(this.options.claraUUID, null, { waitForPublish: true });
 
-      console.log("Making custom configurator...");
+      /*console.log("Making custom configurator...");
       console.log(this.options.optionConfig);
 
       var optionObj = this.options.optionConfig.options;
@@ -64,7 +128,7 @@ define([
         this.element.append('</div>');
         optionCounter++;
       }
-      console.log("done");
+      console.log("done");*/
     }
 
   });
