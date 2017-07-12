3ab59c8ec8dac30af5a79940102948d3b567c3ca
Daniel <lanigela@gmail.com>
2017-07-12 14:43:16 -0400

    move the configurator code to player

--- web/js/clara-player.js
+++ web/js/clara-player.js
@@ -10,7 +10,8 @@ define([
 ], function($, claraPlayer) {
   $.widget('clara.player', {
     options: {
-
+      optionConfig: null,
+      claraUUID: ''
     },
 
     _init: function () {
@@ -18,13 +19,16 @@ define([
     },
 
     _create: function () {
+      console.log("ClaraUUID=" + this.options.claraUUID);
+      console.log(this.options.optionConfig);
+      /*
       var clara = claraPlayer('clara-player');
       clara.on('loaded', function() {
         console.log('Clara player is loaded and ready');
       });
 
       // Fetch and initialize the sceneId
-      clara.sceneIO.fetchAndUse("6cfce43b-fd40-4624-bee6-7f911e9405dd");
+      clara.sceneIO.fetchAndUse("6cfce43b-fd40-4624-bee6-7f911e9405dd");*/
     },
 
     _OnClick: function ($this, $widget) {
